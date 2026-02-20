44f0f701efcdf0a61fbbc776070c5f4b780b36a9
Larzous <larzous@gmail.com>
2026-02-20 17:05:47 -0500

    fix dockerfile per next lesson

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,4 +1,9 @@
 FROM debian:stable-slim
+
+RUN apt update
+RUN apt upgrade -y
+RUN apt install python3 -y
+
 COPY main.py main.py
 COPY books/ books/
-CMD ["python", "main.py"]
+CMD ["python3", "main.py"]
